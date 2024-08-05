a685aa7360c79f2260fb7429af5123f4c32ea636
Bianca <biancalbarbosa@hotmail.com>
2024-08-05 13:25:30 -0300

    feat: show book count on search

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import {
-  EMPTY,
   catchError,
   debounceTime,
-  distinctUntilChanged,
   filter,
   map,
+  of,
   switchMap,
   throwError,
 } from 'rxjs';
-import { Item } from 'src/app/models/interfaces';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -24,26 +23,26 @@ const PAUSA = 1000;
 export class ListaLivrosComponent {
   campoBusca = new FormControl();
   mensagemErro = '';
+  livrosResultado: LivrosResultado;
+  listaLivros: LivroVolumeInfo[];
 
   constructor(private service: LivroService) {}
 
   livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
     debounceTime(PAUSA),
     filter((valorDigitado) => valorDigitado.length >= 3),
-    distinctUntilChanged(),
     switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
-    map((items) => this.livrosResultadoParaLivros(items)),
-    catchError((error) => {
-      console.log(error);
-      this.mensagemErro = 'Ops, ocorreu um erro. Recarregue a aplicação!';
-      return EMPTY;
-      // return throwError(
-      //   () =>
-      //     new Error(
-      //       (this.mensagemErro =
-      //         'Ops, ocorreu um erro. Recarregue a aplicação!')
-      //     )
-      // );
+    map((resultado) => (this.livrosResultado = resultado)),
+    map((resultado) => resultado.items ?? []),
+    map((items) => (this.listaLivros = this.livrosResultadoParaLivros(items))),
+    catchError((erro) => {
+      console.log(erro);
+      return throwError(
+        () =>
+          new Error(
+            (this.mensagemErro = `Ops, ocorreu um erro! Recarregue a aplicação!`)
+          )
+      );
     })
   );
 
